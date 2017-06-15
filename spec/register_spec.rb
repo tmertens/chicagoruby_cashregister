0630e07908b3443d92b265d88e0bf74120bc2fdc
Tangible Dream <darclan28@yahoo.com>
2017-06-15 00:35:40 +0000

    failtest

--- spec/register_spec.rb
+++ spec/register_spec.rb
@@ -3,11 +3,11 @@ require_relative '../register'
 RSpec.describe 'A cash register' do
   it 'takes a list of products as input and returns the subtotals' do
     product_list = [
-        { product: 'Crackers', price: 3.47 },
-        { product: 'Red Wine', price: 13.99 },
-        { product: 'Cheese', price: 6.98 }
+        { product: 'Crackers', price: 3.47,   type: 'b' },
+        { product: 'Red Wine', price: 13.99,  type: 'v' },
+        { product: 'Cheese',   price: 6.98,   type: 'b' }
     ]
     total = Register.total(product_list)
-    expect(total).to eql 24.44
+    expect(total).to eql 24.43
   end
 end
