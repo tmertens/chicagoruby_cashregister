932c6978f0c57490c5c5f42e06e9af44c00ae278
Tangible Dream <darclan28@yahoo.com>
2017-06-15 01:06:52 +0000

    added calculate tax spec and function

--- register.rb
+++ register.rb
@@ -3,4 +3,10 @@ module Register
   def self.total(items)
     items.inject(0) { |sum, item| sum + item[:price] }
   end
+  def self.get_tax_rate(tax_type)
+    tax_type == 'v' ? 0.05 : 0.03
+  end
+  def self.calculate_tax(item)
+    item[:price] * get_tax_rate(item[:tax_type])
+  end
 end
--- spec/register_spec.rb
+++ spec/register_spec.rb
@@ -3,11 +3,16 @@ require_relative '../register'
 RSpec.describe 'A cash register' do
   it 'takes a list of products as input and returns the subtotals' do
     product_list = [
-        { product: 'Crackers', price: 3.47,   type: 'b' },
-        { product: 'Red Wine', price: 13.99,  type: 'v' },
-        { product: 'Cheese',   price: 6.98,   type: 'b' }
+        { product: 'Crackers', price: 3.47,   tax_type: 'b' },
+        { product: 'Red Wine', price: 13.99,  tax_type: 'v' },
+        { product: 'Cheese',   price: 6.98,   tax_type: 'b' }
     ]
     total = Register.total(product_list)
     expect(total).to eql 24.44
+  end
+  it 'takes an regular item and adds proper tax' do
+    product = { product: 'Crackers', price: 3.47,   tax_type: 'b' }
+    total = Register.calculate_tax(product)
+    expect(total).to eql 3.47*0.03
   end
 end
